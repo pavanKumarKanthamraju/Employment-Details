0b4ab9ce714e99592d78d8bee9e343dd8b388e67
pavan kumar <pavanhope14@gmail.com>
2025-11-12 14:23:32 +0530

    config files updated.

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageReporters: ['json-summary','text', 'html'],
   reporters: [
-    'default',
+   'default',
     ['jest-html-reporter', {
       pageTitle: 'Hybrid Test Report',
       outputPath: 'utreports/test-report.html',
